53f79b99f51eaaa4151ef25ed9d3f893414b8b5f
Sebastian <seebzei@gmail.com>
2018-03-29 16:39:18 +0200

    Add ability to draw map boxes

---
+++ Assets/Scripts/MapEditor.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEditor : MonoBehaviour
+{
+
+	private TileHandler _tileHandler;
+
+	public GameObject Obstacle;
+	public GameObject ObstacleParent;
+
+	private Plane _mapPlane;
+	private Ray _screenToWorldRay;
+	private float _distance;
+	private Vector2 _lastEditedTile = Vector2.zero;
+	void Start () 
+	{
+		_mapPlane = new Plane(Vector3.up, Vector3.zero);
+		_tileHandler = GetComponent<TileHandler>();
+	}
+	
+	void Update () 
+	{
+		//Left mouse button clicked
+        if (Input.GetMouseButton(0))
+		{
+			EditObstacle();
+		}
+	}
+
+	/// <summary>
+	/// Adds or removes obstacle from tile where mouse clicked
+	/// </summary>
+	private void EditObstacle()
+	{
+		Vector2 tile = MousePositionToTile();
+		
+		if(tile == _lastEditedTile)
+		{
+			return;
+		}
+
+		if(_tileHandler.IsTileOccupied(tile))
+		{
+			_tileHandler.RemoveTile(tile);
+		}else
+		{	
+			_tileHandler.AddTile(tile, CreateObstacle(tile));
+		}
+
+		_lastEditedTile = tile;
+	}
+
+	/// <summary>
+	/// Returns the closest tile from mouse position
+	/// </summary>
+	/// <returns></returns>
+	private Vector2 MousePositionToTile()
+	{
+		_screenToWorldRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if(_mapPlane.Raycast(_screenToWorldRay, out _distance))
+		{
+			Vector3 position = _screenToWorldRay.GetPoint(_distance);
+			return _tileHandler.ClosestTile(position);
+		}
+		return Vector2.zero; //TODO: Throw exception
+	}
+
+	/// <summary>
+	/// Creates an obstacle gameobject
+	/// </summary>
+	/// <returns></returns>
+	private GameObject CreateObstacle(Vector2 tile)
+	{
+		GameObject obstacle = Instantiate(Obstacle, new Vector3(tile.x, -.5f, tile.y), Quaternion.identity);
+		obstacle.transform.SetParent(ObstacleParent.transform);
+		obstacle.name = "Obstacle";
+		return obstacle;
+	}
+}
---
+++ Assets/Scripts/TileHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileHandler : MonoBehaviour 
+{
+
+	public Hashtable OccupiedTiles { get; private set; }
+	void Start () 
+	{
+		OccupiedTiles = new Hashtable();
+	}
+
+	/// <summary>
+	/// Returnst true if the given tile is already occupied by another tile
+	/// </summary>
+	/// <param name="tile"></param>
+	/// <returns></returns>
+	public bool IsTileOccupied(Vector2 tile)
+	{
+		return OccupiedTiles.ContainsKey(tile) ? true : false;
+	}
+
+	/// <summary>
+	/// Returns the closest tile to a given posistion
+	/// </summary>
+	/// <param name="position"></param>
+	/// <returns></returns>
+	public Vector2 ClosestTile(Vector3 position)
+	{
+		return new Vector2(Mathf.Round(position.x), Mathf.Round(position.z));
+	}
+
+	/// <summary>
+	/// Adds given tile to OccupiedTiles table
+	/// </summary>
+	public void AddTile(Vector2 tile, GameObject obstacle)
+	{
+		if(IsTileOccupied(tile))
+		{
+			return;
+		}
+
+		OccupiedTiles.Add(tile, obstacle);
+	}
+
+	/// <summary>
+	/// Removes given tile from occupied table
+	/// </summary>
+	/// <param name="tile"></param>
+	public void RemoveTile(Vector2 tile)
+	{
+		if(IsTileOccupied(tile))
+		{
+			Destroy(OccupiedTiles[tile] as GameObject);
+			OccupiedTiles.Remove(tile);
+		}
+	}
+}
